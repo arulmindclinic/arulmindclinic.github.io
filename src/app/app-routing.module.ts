bf15f31d7a37329faa71c235abc219c2276e0af4
pvetrieng@gmail.com <pvetrieng@gmail.com>
2021-08-19 09:28:18 -0700

    upgrade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
